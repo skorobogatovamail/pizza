aa3760bdd45ffb71905105ffb11b3e7e3f825d4b
skorobogatovamail <skorobogatovamail@gmail.com>
2025-01-19 12:45:35 +0300

    add CheckboxFiltersGroup

--- components/shared/CheckboxFiltersGroup.tsx
+++ components/shared/CheckboxFiltersGroup.tsx
@@ -3,6 +3,7 @@
 import { cn } from "@/lib/utils";
 import React, { useState } from "react";
 import { Title } from "./Title";
+import { FiltersCheckbox } from "./FiltersCheckbox";
 
 interface Item {
   text: string;
@@ -32,7 +33,9 @@ export const CheckboxFiltersGroup: React.FC<Props> = ({
       <Title text={title || "Ингредиенты"} className="font-bold mb-5"></Title>
       <div className={cn("flex flex-col gap-4", className)}>
         {itemsToMap?.map((el) => {
-          return <div key={el.value}>{el.text}</div>;
+          return (
+            <FiltersCheckbox key={el.value} text={el.text} value={el.value} />
+          );
         })}
       </div>
       <button onClick={(prev) => setCollapsed(!prev)}>+</button>
