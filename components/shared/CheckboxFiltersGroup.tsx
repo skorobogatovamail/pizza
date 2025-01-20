9b5a3c00556ef937696e984330df23f72ca15d3b
Elena Skorobogatova <skorobogatovamail@gmail.com>
2025-01-20 09:15:49 +0300

    add CheckboxFiltersGroup

--- components/shared/CheckboxFiltersGroup.tsx
+++ components/shared/CheckboxFiltersGroup.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Title } from "./Title";
 import { FiltersCheckbox } from "./FiltersCheckbox";
+import { Input } from "../ui/input";
+
 
 interface Item {
   text: string;
@@ -13,32 +15,42 @@ interface Item {
 interface Props {
   className?: string;
   title?: string;
-  items?: Item[];
-  defaultItems?: Item[];
+  items: Item[];
   limit?: number;
 }
 
 export const CheckboxFiltersGroup: React.FC<Props> = ({
   className,
-  title,
+  title = "Ингредиенты",
   items,
-  defaultItems,
-  limit,
+  limit = 6,
 }) => {
   const [collapsed, setCollapsed] = useState(true);
-  const itemsToMap = items ? items : defaultItems;
+  const [itemsToMap, setitemsToMap] = useState(items?.slice(0, limit));
+
+  useEffect(
+    () => collapsed ? setitemsToMap(items?.slice(0, limit)) : setitemsToMap(items),
+    [collapsed, items, limit]
+  )
 
   return (
-    <>
-      <Title text={title || "Ингредиенты"} className="font-bold mb-5"></Title>
-      <div className={cn("flex flex-col gap-4", className)}>
+
+    <div className="pt-5">
+      <Title text={title} className="font-bold mb-5"></Title>
+      {!collapsed && <div className="mb-5">
+        <Input type="string" placeholder="Поиск" className="bg-gray-50 border-none"></Input>
+      </div>}
+      <div className={cn("flex flex-col gap-4 max-h-96 pr-2 overflow-auto srollbar", className)}>
         {itemsToMap?.map((el) => {
           return (
             <FiltersCheckbox key={el.value} text={el.text} value={el.value} />
           );
         })}
       </div>
-      <button onClick={(prev) => setCollapsed(!prev)}>+</button>
-    </>
+      <button className="text-primary mt-3" onClick={() => setCollapsed((prev) => !prev)
+      }>{collapsed ? '+ Раскрыть' : 'Скрыть'}</button>
+    </div>
+
+
   );
 };
--- components/shared/Filters.tsx
+++ components/shared/Filters.tsx
@@ -8,47 +8,46 @@ import { CheckboxFiltersGroup } from "./CheckboxFiltersGroup";
 import { defaultItems, items } from "@/lib/constants/items";
 
 interface Props {
-  className?: string;
+    className?: string;
 }
 
 export const Filters: React.FC<Props> = ({ className }) => {
-  return (
-    <div className={cn(className)}>
-      <Title text="Фильтрация" className="font-bold mb-5"></Title>
-      <div className="flex flex-col gap-4">
-        <FiltersCheckbox text="Можно собирать" value="1" />
-        <FiltersCheckbox text="Новинки" value="2" />
-      </div>
+    return (
+        <div className={cn(className)}>
+            <Title text="Фильтрация" className="font-bold mb-5"></Title>
+            <div className="flex flex-col gap-4">
+                <FiltersCheckbox text="Можно собирать" value="1" />
+                <FiltersCheckbox text="Новинки" value="2" />
+            </div>
 
-      <div className="mt-5">
-        <div className="w-fill h-[1px] bg-gray-200"></div>
-        <p className="font-bold mb-3 mt-5">Цена от и до</p>
-        <div className="flex gap-4 my-5">
-          <Input
-            type="number"
-            placeholder="0"
-            min={0}
-            max={30000}
-            defaultValue={0}
-          ></Input>
-          <Input
-            type="number"
-            placeholder="30000"
-            min={100}
-            max={30000}
-          ></Input>
+            <div className="mt-5">
+                <div className="w-fill h-[1px] bg-gray-200"></div>
+                <p className="font-bold mb-3 mt-5">Цена от и до</p>
+                <div className="flex gap-4 my-5">
+                    <Input
+                        type="number"
+                        placeholder="0"
+                        min={0}
+                        max={30000}
+                        defaultValue={0}
+                    ></Input>
+                    <Input
+                        type="number"
+                        placeholder="30000"
+                        min={100}
+                        max={30000}
+                    ></Input>
+                </div>
+                <RangeSlider min={0} max={30000} step={100}></RangeSlider>
+            </div>
+
+            <div className="mt-5">
+                <CheckboxFiltersGroup
+                    title="Ингредиенты"
+                    items={items}
+                    limit={6}
+                />
+            </div>
         </div>
-        <RangeSlider min={0} max={30000} step={100}></RangeSlider>
-      </div>
-
-      <div className="mt-5">
-        <CheckboxFiltersGroup
-          title="Ингредиенты"
-          defaultItems={defaultItems}
-          items={items}
-          limit={6}
-        />
-      </div>
-    </div>
-  );
+    );
 };
