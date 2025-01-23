bcc0f99e543f62914061ad76ba9acb5481d7f4e4
Elena Skorobogatova <skorobogatovamail@gmail.com>
2025-01-23 19:04:37 +0300

    add routes

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,14 @@
-import { NextResponse } from "next/server";
+import { prisma } from "@/prisma/prisma-client";
+import { NextRequest, NextResponse } from "next/server";
 
-export function GET() {
-    return NextResponse.json({
-        users: ['user1', 'user2', 'user3']
-    })
+export async function GET() {
+    const users = await prisma.user.findMany();
+    return NextResponse.json(users);
+}
+
+export async function POST(req: NextRequest) {
+    const data = req.json();
+    const user = await prisma.user.create({ data });
+    return NextResponse.json(user);
+
 }
