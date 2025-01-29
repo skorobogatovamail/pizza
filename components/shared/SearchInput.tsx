cab4611153f1c7302367d63db2fc28130d83a198
Elena Skorobogatova <skorobogatovamail@gmail.com>
2025-01-29 23:35:45 +0300

    add input

--- components/shared/SearchInput.tsx
+++ components/shared/SearchInput.tsx
@@ -8,7 +8,8 @@ interface Props {
 export const SearchInput: React.FC<Props> = ({ className }) => {
     return <div className={cn('', className)}>
         <div className="flex rounded-2xl justify-between flex-1 relative h-11">
-            <Search className="text-gray-400" />
+            <Search className="text-gray-400 absolute left-3 top-1/2 translate-y-[-50%] h-5" />
+            <input className="rounded-2xl outline-none w-full bg-gray-100 pl-11" type="text" placeholder="Найди свою пиццу" />
         </div>
     </div>
 }
