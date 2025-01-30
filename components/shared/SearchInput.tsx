18515562dd77167f1be0fab115cd4c243670c0a5
Elena Skorobogatova <skorobogatovamail@gmail.com>
2025-01-30 22:23:29 +0300

    add focused input

--- components/shared/SearchInput.tsx
+++ components/shared/SearchInput.tsx
@@ -2,7 +2,8 @@
 
 import { cn } from "@/lib/utils";
 import { Search } from "lucide-react";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { useClickAway } from "react-use";
 
 interface Props {
     className?: string;
@@ -11,11 +12,16 @@ interface Props {
 export const SearchInput: React.FC<Props> = ({ className }) => {
 
     const [focused, setFocused] = useState(false)
+    const ref = useRef(null)
 
-    return <div className={cn('', className)}>
+    useClickAway(ref, () => {
+        setFocused(false)
+    })
+
+    return (
         <>
-            {focused && <div className="fixed top=0 left=0  bottom-0 right-0 bg-black/50 z-30 "></div>}
-            <div className="flex rounded-2xl justify-between flex-1 relative h-11">
+            {focused && <div className="fixed top-0 left-0  bottom-0 right-0 bg-black/50 z-31 "></div>}
+            <div ref={ref} className="flex rounded-2xl justify-between flex-1 relative h-11">
                 <Search className="text-gray-400 absolute left-3 top-1/2 translate-y-[-50%] h-5" />
                 <input className="rounded-2xl outline-none w-full bg-gray-100 pl-11 z-30"
                     type="text"
@@ -24,6 +30,5 @@ export const SearchInput: React.FC<Props> = ({ className }) => {
                 />
             </div>
         </>
-
-    </div>
+    )
 }
