720f83cbea56d447f47a575901ff9ba8f425da15
Elena Skorobogatova <skorobogatovamail@gmail.com>
2025-01-30 22:45:55 +0300

    add popup

--- components/shared/SearchInput.tsx
+++ components/shared/SearchInput.tsx
@@ -20,7 +20,7 @@ export const SearchInput: React.FC<Props> = ({ className }) => {
 
     return (
         <>
-            {focused && <div className="fixed top-0 left-0  bottom-0 right-0 bg-black/50 z-31 "></div>}
+            {focused && <div className="fixed top-0 left-0  bottom-0 right-0 bg-black/50 z-30 "></div>}
             <div ref={ref} className="flex rounded-2xl justify-between flex-1 relative h-11">
                 <Search className="text-gray-400 absolute left-3 top-1/2 translate-y-[-50%] h-5" />
                 <input className="rounded-2xl outline-none w-full bg-gray-100 pl-11 z-30"
@@ -28,6 +28,12 @@ export const SearchInput: React.FC<Props> = ({ className }) => {
                     placeholder="Найди свою пиццу"
                     onFocus={() => setFocused(true)}
                 />
+            </div>
+
+            <div className="">
+                <Link>
+                    <span></span>
+                </Link>
             </div>
         </>
     )
--- components/shared/TopBar.tsx
+++ components/shared/TopBar.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export const TopBar: React.FC<Props> = ({ className }) => {
   return (
-    <div className="sticky top-0 bg-white py-5 shadow-lg shadow-black/5">
+    <div className="sticky top-0 bg-white py-5 shadow-lg shadow-black/5 z-10">
       <Container className="flex items-center justify-between" >
         <Categories />
         <SortPopup />
