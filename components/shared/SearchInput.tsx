2223ff3ce8c6c1d0575f2f0b3d6ecf0e5c384ec1
Elena Skorobogatova <skorobogatovamail@gmail.com>
2025-02-01 00:48:59 +0300

    add search

--- components/shared/SearchInput.tsx
+++ components/shared/SearchInput.tsx
@@ -2,9 +2,10 @@
 
 import { cn } from "@/lib/utils";
 import { Api } from "@/services/api-client";
+import { Product } from "@prisma/client";
 import { Search } from "lucide-react";
 import Link from "next/link";
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useClickAway } from "react-use";
 
 interface Props {
@@ -14,11 +15,11 @@ interface Props {
 export const SearchInput: React.FC<Props> = ({ className }) => {
 
     const [focused, setFocused] = useState(false)
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useState<Product[]>([])
     const [searchQuery, setSearchQuery] = useState('')
     const ref = useRef(null)
 
-    useEffect(() => { Api.products.search(searchQuery) }, [searchQuery])
+    useEffect(() => { Api.products.search(searchQuery).then((data) => setProducts(data)) }, [searchQuery])
 
     useClickAway(ref, () => {
         setFocused(false)
@@ -39,13 +40,17 @@ export const SearchInput: React.FC<Props> = ({ className }) => {
                 <div className={cn("absolute w-full bg-white rounded-2xl top-14 py-2 z-30 shadow-md transition-all duration-200 invisible opacity-0",
                     focused && "visible opacity-100 top-12"
                 )}>
-                    <Link href='/product/1' className="px-3 py-2 hover:bg-primary/10 cursor-pointer flex items-center gap-3">
-                        <img src='/assets/pizza1.png' alt='pizza1' className="h-8 w-8"></img>
-                        <div >
-                            Пицца 1
-                        </div>
-                    </Link>
 
+                    {products.map(el => {
+                        return (
+                            <React.Fragment key={el.id}>
+                                <Link href={`/product/${el.id}`} className="px-3 py-2 hover:bg-primary/10 cursor-pointer flex items-center gap-3">
+                                    <img src={el.imageUrl} alt={el.name} className="h-8 w-8"></img>
+                                    <div >{el.name}</div>
+                                </Link>
+                            </React.Fragment>
+                        )
+                    })}
                 </div>
             </div>
         </>
--- services/products.ts
+++ services/products.ts
@@ -1,7 +1,8 @@
 import { Product } from "@prisma/client"
 import { axiosInstance } from "./instance"
+import { ApiRoutes } from "./constants";
 
 export const search = async (query: string) => {
-    const { data } = await axiosInstance.get<Product>('/products/search/', { params: { query } });
+    const { data } = await axiosInstance.get<Product[]>(ApiRoutes.SEARCH_PRODUCTS, { params: { query } });
     return data;
 }
