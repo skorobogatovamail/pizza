c9ca9c00c7aa0d7b99b6ea800295eec656fdb9e0
Elena Skorobogatova <skorobogatovamail@gmail.com>
2025-02-18 09:16:34 +0300

    add group variants

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { Container } from "@/components/shared/Container";
+import { GroupVariants } from "@/components/shared/GroupVariants";
 import { ProductImage } from "@/components/shared/ProductImage";
 import { Title } from "@/components/shared/Title";
 import { prisma } from "@/prisma/prisma-client";
@@ -24,6 +25,20 @@ export default async function productPage({ params }: { params: { id: string } }
                 <div className="w-[490px] bg-[#FCFCFC] p-7">
                     <Title text={product.name} size="md" className="font-extrabold mb-1"></Title>
                     <p className="text-gray-400">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id dignissimos, ut nostrum, dolor eum quos numquam fuga magni, hic aperiam odit! Ea quisquam tenetur, officia velit similique odit delectus tempora.</p>
+                    <GroupVariants items={[
+                        {
+                            name: 'Маленькая пицца',
+                            value: 1
+                        },
+                        {
+                            name: 'Средняя пицца',
+                            value: 2
+                        },
+                        {
+                            name: 'Большая пицца',
+                            value: 3
+                        },
+                    ]} />
                 </div>
             </div>
 
